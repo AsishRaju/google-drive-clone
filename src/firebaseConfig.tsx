06144ceef1e5cb0638367fe87659abcd6d6b3d16
AsishRaju <vashish888@gmail.com>
2023-11-25 23:33:44 -0800

    added package.json

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -4,13 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4L27n9mGVRkAzdB8CXfYZoLnbobooLUA",
-  authDomain: "drive-clone-ab4c6.firebaseapp.com",
-  projectId: "drive-clone-ab4c6",
-  storageBucket: "drive-clone-ab4c6.appspot.com",
-  messagingSenderId: "841798489358",
-  appId: "1:841798489358:web:541aa5cc0367a3689a335f",
-  measurementId: "G-6JZWYJPKEZ",
+  apiKey: "AIzaSyCf8sMXiRQNbDJ9cyGf_7FqdEQkLpPGDyE",
+  authDomain: "guardian-b2282.firebaseapp.com",
+  projectId: "guardian-b2282",
+  storageBucket: "guardian-b2282.appspot.com",
+  messagingSenderId: "869212715133",
+  appId: "1:869212715133:web:0aa2b1d10c413de7449914"
 };
 
 // Initialize Firebase
